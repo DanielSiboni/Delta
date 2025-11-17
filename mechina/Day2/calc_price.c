85be865345c3dd7e6917f4401e8fe72be2f268fe
Daniel SIboni <danieli.siboni@gmail.com>
2025-11-17 14:55:23 +0200

    fixed the third assignment from day 2 in the mechina (calc_price), added defines.

--- mechina/Day2/calc_price.c
+++ mechina/Day2/calc_price.c
@@ -8,7 +8,11 @@
 
 #define MM_TO_CM ((float)1/10)
 #define G_TO_KG ((float)1/1000)
-#define AGOROT_TO_SHEKEL ((float)1/100)
+
+#define FIXED_SHIP_FEE ((float)11/2)
+#define COST_PER_KG (11)
+#define COST_PER_CMS ((float)3/100)
+#define COST_PER_CMS_BY_KG ((float)1/100)
 
 /*------------------------------------------------------
 * Function Name - main
@@ -35,7 +39,7 @@ int main() {
 
     float area = (length * MM_TO_CM) * (width * MM_TO_CM);
     float kg_weight = weight * G_TO_KG;
-    float cost = 5.5 + (11 * (kg_weight * G_TO_KG)) + ((3 * AGOROT_TO_SHEKEL) * area) + ((1 * AGOROT_TO_SHEKEL) * (area / kg_weight));
+    float cost = FIXED_SHIP_FEE + (COST_PER_KG * (kg_weight * G_TO_KG)) + (COST_PER_CMS * area) + (COST_PER_CMS_BY_KG * (area / kg_weight));
 
     printf("the size of the box: %.2f cm\n", area);
     printf("the weight of the box: %.3f kg\n", kg_weight);
